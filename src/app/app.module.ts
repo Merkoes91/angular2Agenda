10341cb5edfe4fc592e9bb7531830e48dbe3e90b
Marko de roos <Marko de roos>
2016-10-08 20:34:05 +0200

    trying out angular2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
+import { GridComponentComponent } from './grid-component/grid-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponentComponent,
+    GridComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/grid-component/grid-component.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { GridComponentComponent } from './grid-component.component';
+
+describe('Component: GridComponent', () => {
+  it('should create an instance', () => {
+    let component = new GridComponentComponent();
+    expect(component).toBeTruthy();
+  });
+});
